a9ec9834725217da020f698791ee8ac58363a409
Somraj Mukherjee <somraj.mukherjee@yocket.in>
2023-08-22 20:49:40 +0530

    👌 IMPROVE: added brand and action sections

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,12 +5,13 @@ const Navbar = async () => {
   return (
     <div className='fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2'>
       <div className='container max-w-7xl h-full mx-auto flex items-center justify-between gap-2'>
-        {/* logo */}
+        {/* brand */}
         <Link
           href='/'
           className='flex gap-2 items-center'>
           <p className=' text-zinc-700 text-sm font-medium'>Task Planner Pro</p>
         </Link>
+        {/* brand */}
 
         {/* actions */}
         <Link
@@ -18,6 +19,7 @@ const Navbar = async () => {
           className={buttonVariants()}>
           Sign In
         </Link>
+        {/* actions */}
       </div>
     </div>
   );
