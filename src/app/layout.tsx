ba91ee7d145d8aec68f34e815ea1c4b3351778de
Somraj Mukherjee <somraj.mukherjee@yocket.in>
2023-08-22 20:49:01 +0530

    👌 IMPROVE: added navbar and basic sign in button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
+import Navbar from '@/components/Navbar';
 import { cn } from '@/lib/utils';
-import '@/styles/globals.css'
+import '@/styles/globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
@@ -15,6 +16,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html
       lang='en'
       className={cn('bg-white text-slate-900 antialiased light', inter.className)}>
+      <Navbar />
       <body className='min-h-screen pt-12 bg-slate-50 antialiased'>
         <div className='container max-w-7xl mx-auto h-full pt-12'>{children}</div>
       </body>
